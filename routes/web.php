ee3e936da20394701db69f2a6d222ec691ec77b3
belsy05 <belsy.mairena@unah.hn>
2021-12-08 02:44:38 -0600

    Rutas Alumno

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* ********************************************** Alumno *********************************/
+//ruta para leer
+Route::get('/alumnos', [AlumnoController::class, 'index'])
+->name('alumno.index');
+
+//ruta para barra de busqueda
+Route::get('/alumnos/buscar', [AlumnoController::class, 'index2'])
+->name('alumno.index2');
+
+//ruta para mostrar
+Route::get('/alumnos/{id}', [AlumnoController::class, 'show'])
+->name('alumno.mostrar')->where('id', '[0-9]+');
+
+//ruta para crear
+Route::get('/alumnos/crear', [AlumnoController::class, 'crear'])
+->name('alumno.crear');
+
+//ruta para postear
+Route::post('/alumnos/crear', [AlumnoController::class, 'store'])
+->name('alumno.guardar');
+
+//ruta para mostrar formulario
+Route::get('/alumnos/{id}/editar', [AlumnoController::class, 'edit'])
+->name('alumno.edit')->where('id', '[0-9]+');
+
+//para actualizar los datos
+Route::put('/alumnos/{id}/editar', [AlumnoController::class, 'update'])
+->name('alumno.update')->where('id', '[0-9]+');
+
+//ruta para borrar datos
+Route::delete('/alumnos/{id}/borrar', [AlumnoController::class, 'destroy'])
+->name('alumno.borrar')->where('id', '[0-9]+');
